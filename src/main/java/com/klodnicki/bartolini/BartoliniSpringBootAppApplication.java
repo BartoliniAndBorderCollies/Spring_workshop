5f0eee07da5286acd08f35b8177c546ad34967bb
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-01 17:11:40 +0200

    add main

---
+++ src/main/java/com/klodnicki/bartolini/BartoliniSpringBootAppApplication.java
@@ -0,0 +1,16 @@
+package com.klodnicki.bartolini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// @Configuration
+// @EnableAutoConfiguration
+// @ComponentScan
+public class BartoliniSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BartoliniSpringBootAppApplication.class, args);
+	}
+
+}
