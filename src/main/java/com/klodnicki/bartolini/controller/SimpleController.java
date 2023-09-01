57cabc85dc7b4ca9a14a479d0ea6ebe0a44e87b3
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-01 17:13:14 +0200

    add class to practice Thymeleaf

---
+++ src/main/java/com/klodnicki/bartolini/controller/SimpleController.java
@@ -0,0 +1,20 @@
+package com.klodnicki.bartolini.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SimpleController {
+
+    @Value("${author.name}")
+    String myName;
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        // zapytanie do serwisu i do bazy danych o imię jakies losowe
+        model.addAttribute("myName", myName);
+        return "home";
+    }
+}
