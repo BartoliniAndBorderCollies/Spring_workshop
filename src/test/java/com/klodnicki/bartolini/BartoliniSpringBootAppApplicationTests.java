cbe14b6a5324a7c5b022ee768fedb4555e547e43
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-01 17:12:08 +0200

    add test class

---
+++ src/test/java/com/klodnicki/bartolini/BartoliniSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klodnicki.bartolini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BartoliniSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
