1123cb8adfcbc7f4de937836b18144223c0fc833
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-02 19:12:18 +0200

    add thymeleaf values so that it takes text from properties file

--- src/main/java/com/klodnicki/bartolini/controller/Homework.java
+++ src/main/java/com/klodnicki/bartolini/controller/Homework.java
@@ -1,6 +1,7 @@
 package com.klodnicki.bartolini.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +9,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Homework {
 
+    @Value("${textA}")
+    String text1;
+    @Value("${textB}")
+    String text2;
+    @Value("${textC}")
+    String text3;
+
     @GetMapping("/homework")
 
     public String homeworkGreeting(Model model) {
-        model.addAttribute("text1", "so GREAT!");
-        model.addAttribute("text2", "learn this special tool!");
-        model.addAttribute("text3", "this works so fine!");
+        model.addAttribute("text1", text1);
+        model.addAttribute("text2", text2);
+        model.addAttribute("text3", text3);
         return "homework";
     }
 }
