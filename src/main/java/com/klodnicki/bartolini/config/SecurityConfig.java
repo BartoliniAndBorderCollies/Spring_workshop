b9f3f80ddd80fc005e20e048b2ed1753af663cdc
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-05 17:11:40 +0200

    add Spring Security config

---
+++ src/main/java/com/klodnicki/bartolini/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.klodnicki.bartolini.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests()
+
+                .requestMatchers("/admin/**")
+                .authenticated()
+
+
+//                .anyRequest()
+
+
+                .and()
+                .build();
+    }
+}
