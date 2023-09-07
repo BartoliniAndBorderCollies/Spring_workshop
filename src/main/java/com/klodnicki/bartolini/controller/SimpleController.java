8d15173e98bae132c5fb954c089983e26e7b1150
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-07 19:54:07 +0200

    add path for controller

--- src/main/java/com/klodnicki/bartolini/controller/SimpleController.java
+++ src/main/java/com/klodnicki/bartolini/controller/SimpleController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class SimpleController {
 
     @Value("${author.name}")
